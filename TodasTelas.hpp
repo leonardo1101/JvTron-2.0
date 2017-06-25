cd2602974597828457f570bbf5bb185db5f15efd
Joao Barbirato <joaobarbirato@gmail.com>
2017-06-25 11:26:25 -0300

    menu e algumas outras coisas

---
+++ TodasTelas.hpp
@@ -0,0 +1,25 @@
+/*
+	JvTron: Trabalho 2
+	Departamento de Computação
+	UFSCar Universidade Federal de São Carlos
+	Disciplina: Estrutura de Dados
+	Professor: Roberto Ferrari
+	Aluno(a):								RA:
+		João Gabriel Melo Barbirato			726546
+		Leonardo de Oliveira Peralta		726556
+		Gabrieli Santos						726523
+    
+	Controle de Versão: https://github.com/leonardo1101/JvTron-2.0
+*/
+
+// Incluir todas as telas
+#ifndef SCREENS_HPP_INCLUDED
+#define SCREENS_HPP_INCLUDED
+
+// Classe mãe
+#include "Tela.hpp"
+
+// Incluir todas as telas
+#include "Menu.hpp"
+#include "Jogo.hpp"
+#endif // fim da inclusão
